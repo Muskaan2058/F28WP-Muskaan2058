1e4919808fa89491fe14588321c4843045656596
Muskaan2058 <mg2058@hw.ac.uk>
2023-09-24 21:57:29 +0400

    edited placeholder for sticky header

--- week2/script.js
+++ week2/script.js
@@ -5,7 +5,9 @@ const headerPlaceholder = document.getElementById("header-placeholder");
 window.addEventListener("scroll", function() {
   if (window.scrollY > headerPlaceholder.clientHeight) {
     header.classList.add("sticky");
+    headerPlaceholder.style.display = "block"; // Show the placeholder
   } else {
     header.classList.remove("sticky");
+    headerPlaceholder.style.display = "none"; // Hide the placeholder
   }
 });
