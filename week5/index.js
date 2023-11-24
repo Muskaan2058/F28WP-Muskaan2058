93c5c731157884280f197ce6bafae1259fb56888
Muskaan2058 <mg2058@hw.ac.uk>
2023-11-25 00:56:13 +0400

    part4 - video

--- week5/index.js
+++ week5/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
+const port = 4050;
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config({path:'./.env'});
+const path = require('path');
 
 const app = express();
 const db = mysql.createConnection(
     {
-        host : 'localhost',
-        user: 'root',
-        password: '',
-        database: ''
+        host : process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE
     }
-)
+);
 
-const port = 4000;
-app.get('/', (req, res) => {
-    res.send("<h1>Home Page</h1>")
+db.connect((error) => {
+   if(error){
+    console.log(error);
+   }
+   else{
+    console.log('Mysql Connected...');
+   }
 });
+
+const publicDirectory = path.join(__dirname, './public');
+
+app.use(express.static(publicDirectory));
+app.set('view engine', 'hbs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
 
 app.listen(port, () => {
     console.log("Server is started on port " + port);
